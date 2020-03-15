c9e2652f9baa62540b9b0f41e7e46e01ce300662
8c6794b6 <8c6794b6@gmail.com>
2020-03-15 19:06:07 +0900

    Minor tidy up
    Modify header to refer LICENSE file.
    
    Add file local variables.

--- test/finkel-mode-test.el
+++ test/finkel-mode-test.el
@@ -1,27 +1,15 @@
 ;;; finkel-mode-test.el --- Test for finkel-mode -*-lexical-binding:t-*-
 
-;; CopyRight (C) 2020 8c6794b6
+;; Copyright 2017 The finkel-mode Authors.  All rights reserved.
+;;
+;; Use of this source code is governed by a BSD-style license that can be found
+;; in the LICENSE file.
 
 ;; Author: 8c6794b6
+;; Created: Mar 2020
+;; Keywords: test
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.3"))
-;; Created: Mar 2020
-;; Keywords: languages
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
@@ -223,6 +211,9 @@
 (provide 'finkel-mode-test)
 
 ;;; Local Variables:
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; fill-column: 80
 ;;; eval: (buttercup-minor-mode)
 ;;; End:
 
